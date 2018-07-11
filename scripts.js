e243395ce8d6fe176ce1ed51c549b596659194cb
Shakira D <4237135+shakira91@users.noreply.github.com>
2018-07-10 22:29:12 -0400

    wings

--- scripts.js
+++ scripts.js
@@ -7,37 +7,59 @@ class wingClass {
     }
 
     getPrice () {
-        console.log(Math.round(this.price * this.amount));
+        //console.log(Math.round(this.price * this.amount));
     }
 
     getAmount () {
-        console.log(this.amount);
+        //console.log(this.amount);
     }
 
     getCalories () {
-        console.log(81 * this.amount);
+        //console.log(81 * this.amount);
     }
     
     addWingAmountToDom(){
-            for (var i = 0; i <= this.amount; i++) {
+        for (var i = 0; i < this.amount; i++) {
           document.getElementById("bucket").innerHTML += this.img + "\n";
         }     		
     }
 }
 
+var wingCount = 4; 
+document.getElementById("wing-amount").innerHTML = wingCount;
 document.addEventListener("click", function(e) {
-  if (e.target.id == "plain") {
-    var wingOrderPlain = new wingClass(document.getElementById("wing-amount").value, 0.98, "<img src='assets/wing.png'/>");
+    if (e.target.id == "more") {
+        document.getElementById("wing-amount").innerHTML = wingCount = wingCount + 1;
+    } 
+});
+
+
+document.addEventListener("click", function(e) {
+ if (e.target.id == "plain") {
+    var wingOrderPlain = new wingClass(parseInt(document.getElementById("wing-amount").innerHTML), 0.98, "<div class='wing-container'><img class='wing' src='assets/wing.png'/></div>");
+    wingOrderPlain.getAmount();
     wingOrderPlain.addWingAmountToDom();
     wingOrderPlain.getPrice();
     wingOrderPlain.getCalories()
 } else if (e.target.id === "bbq" || e.target.id === "honey-bbq") {
-    var wingOrderBBQ = new wingClass(document.getElementById("wing-amount").value, 1.05);
+    var wingOrderBBQ = new wingClass(document.getElementById("wing-amount").innerHTML, 1.05, "<div class='wing-container'><img class='wing' src='assets/wing_bbq.png'/></div>");
+    wingOrderBBQ.addWingAmountToDom();
     wingOrderBBQ.getPrice();
+    wingOrderBBQ.getCalories()
 } else if (e.target.id === "sweet-chilli") {
     var wingOrderSweetChilli = new wingClass(document.getElementById("wing-amount").value, 1.19);
     wingOrderSweetChilli.getPrice();
-} else {
+} else if (e.target.id === "clear") {
     document.getElementById("bucket").innerHTML = "";
+    wingCount = 4;
+    document.getElementById("wing-amount").innerHTML = wingCount;
 }
+
+if (document.querySelectorAll(".wing").length > 12) {
+    for (var w = 0; w < document.querySelectorAll(".wing").length; w++) {
+        document.querySelectorAll(".wing")[w].style.width = "150px";
+    }
+}
+
 });
+
